34c404ddda550f67aae7208f51d39846e91aae0e
WinterWood0118 <dltjdals125@gmail.com>
2024-02-26 21:31:13 +0900

    성민

--- app/src/main/java/com/example/kotlin_ot_project_i/TeamActivity.kt
+++ app/src/main/java/com/example/kotlin_ot_project_i/TeamActivity.kt
@@ -3,7 +3,6 @@ package com.example.kotlin_ot_project_i
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.ImageButton
 import android.widget.ImageView
 
 class TeamActivity : AppCompatActivity() {
