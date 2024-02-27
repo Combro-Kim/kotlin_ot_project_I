86cfa92e7f76f368ec5e0809249f658e6d7eef1d
Chaemyung Lim <saver203@naver.com>
2024-02-27 19:06:43 +0900

    lim

--- app/src/main/java/com/example/kotlin_ot_project_i/MainActivity.kt
+++ app/src/main/java/com/example/kotlin_ot_project_i/MainActivity.kt
@@ -1,15 +1,40 @@
 package com.example.kotlin_ot_project_i
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import com.example.kotlin_ot_project_i.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         Thread.sleep(1000)
         installSplashScreen()
-        setContentView(R.layout.activity_main)
 
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.imageButton.setOnClickListener {
+            val intent = Intent (this, PersonalActivity::class.java)
+            startActivity(intent)
+        }
+
+        binding.imageButton2.setOnClickListener {
+            val intent = Intent (this, PersonalActivity::class.java)
+            startActivity(intent)
+        }
+
+        binding.imageButton3.setOnClickListener {
+            val intent = Intent (this, PersonalActivity::class.java)
+            startActivity(intent)
+        }
+
+        binding.imageButton4.setOnClickListener {
+            val intent = Intent (this, PersonalActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
