80c8ed2069ed7ff1ddc7e457c85053df44afcbc9
WinterWood0118 <dltjdals125@gmail.com>
2024-02-27 14:44:44 +0900

    성민

--- app/src/main/java/com/example/kotlin_ot_project_i/TeamActivity.kt
+++ app/src/main/java/com/example/kotlin_ot_project_i/TeamActivity.kt
@@ -3,16 +3,17 @@ package com.example.kotlin_ot_project_i
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.ImageView
+import com.example.kotlin_ot_project_i.databinding.ActivityTeamBinding
 
 class TeamActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityTeamBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_team)
+        binding = ActivityTeamBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        val btn_return = findViewById<ImageView>(R.id.returnBtn)
-
-        btn_return.setOnClickListener{
+        binding.teamToolbar.returnBtn.setOnClickListener{
             val intent = Intent(this@TeamActivity, MainActivity::class.java)
             startActivity(intent)
         }
