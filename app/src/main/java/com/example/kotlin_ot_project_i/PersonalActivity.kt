9e2731995af052f87916e9ec6cfb4bb70b4db8ed
WinterWood0118 <dltjdals125@gmail.com>
2024-02-28 12:01:02 +0900

    성민

--- app/src/main/java/com/example/kotlin_ot_project_i/PersonalActivity.kt
+++ app/src/main/java/com/example/kotlin_ot_project_i/PersonalActivity.kt
@@ -3,6 +3,8 @@ package com.example.kotlin_ot_project_i
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.PopupMenu
+import androidx.core.view.isVisible
 import com.example.kotlin_ot_project_i.databinding.ActivityPersonalBinding
 
 class PersonalActivity : AppCompatActivity() {
@@ -61,7 +63,7 @@ class PersonalActivity : AppCompatActivity() {
         setContentView(binding.root)
         val idx = intent.getIntExtra("indexNumber",0)
 
-        binding.ivPersonal.setImageResource(resources.getIdentifier("pic${idx+1}","drawable",packageName))
+        binding.ivPersonal.setBackgroundResource(resources.getIdentifier("pic${idx+1}","drawable",packageName))
         binding.personalNameText.text = getString(nameOfTeam[idx])
         binding.hobbyText.text = getString(hobbyOfTeam[idx])
         binding.mbtiText.text = getString(mbtiOfTeam[idx])
@@ -71,10 +73,58 @@ class PersonalActivity : AppCompatActivity() {
         binding.resolveText.text = getString(resolveOfTeam[idx])
         binding.roleText.text = getString(roleOfTeam[idx])
 
-        binding.personalToolbar.returnBtn.setOnClickListener{
-            val intent = Intent(this, MainActivity::class.java)
-            startActivity(intent)
+        binding.personalToolbar.menuBtn.setOnClickListener{
+            val popupMenu = PopupMenu(applicationContext,it)
+            menuInflater.inflate(R.menu.menu_pop_up,popupMenu.menu)
+            popupMenu.show()
+            popupMenu.setOnMenuItemClickListener{
+                when(it.itemId){
+                    R.id.go_main -> {
+                        val intent = Intent(this, MainActivity::class.java)
+                        startActivity(intent)
+                        return@setOnMenuItemClickListener true
+                    }
+                    R.id.go_team -> {
+                        val intent = Intent(this, TeamActivity::class.java)
+                        startActivity(intent)
+                        return@setOnMenuItemClickListener true
+                    }
+                    R.id.go_member1 -> {
+                        val intent = Intent(this, PersonalActivity::class.java)
+                        intent.putExtra("indexNumber",0)
+                        startActivity(intent)
+                        return@setOnMenuItemClickListener true
+                    }
+                    R.id.go_member2 -> {
+                        val intent = Intent(this, PersonalActivity::class.java)
+                        intent.putExtra("indexNumber",1)
+                        startActivity(intent)
+                        return@setOnMenuItemClickListener true
+                    }
+                    R.id.go_member3 -> {
+                        val intent = Intent(this, PersonalActivity::class.java)
+                        intent.putExtra("indexNumber",2)
+                        startActivity(intent)
+                        return@setOnMenuItemClickListener true
+                    }
+                    R.id.go_member4 -> {
+                        val intent = Intent(this, PersonalActivity::class.java)
+                        intent.putExtra("indexNumber",3)
+                        startActivity(intent)
+                        return@setOnMenuItemClickListener true
+                    }
+                    else-> return@setOnMenuItemClickListener false
+                }
+            }
 
+        }
+
+        binding.personalToolbar.returnBtn.let{
+            it.isVisible = true
+            it.setOnClickListener{
+                val intent = Intent(this, MainActivity::class.java)
+                startActivity(intent)
+            }
         }
     }
 }
